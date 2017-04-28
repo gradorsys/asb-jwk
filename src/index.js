edecddc932bb9c72f7ee6b637fea2ac5d3fb78d7
Gunnar Radzom <rad@Gunnars-MBP-Work.fritz.box>
2017-04-28 03:57:48 +0200

    feat(added adder):

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-const encrypt = (secret, jwk) => `not implemented ${jwk}`;
-// function encrypt(secret, jwk) {
-//     return 'not implemented';
-// }
+module.exports = function myLib() { // Jose, JoseJWE }) {
+  // function jwk2cryptoKey(jwk) {
+  //   const cryptoKey = Jose.Utils.importRsaPublicKey(jwk, 'RSA-OAEP');
+  //   return cryptoKey;
+  // }
 
-function generateKey() {
-  return 'new key';
-}
+  // function encrypt(data, cryptoKey) {
+  //   const cryptographer = new Jose.WebCryptographer();
+  //   const encrypter = new JoseJWE.Encrypter(cryptographer, cryptoKey);
+  //   return encrypter.encrypt(data);
+  // }
 
-module.exports = {
-  encrypt,
-  generateKey,
+  // async function transformData(data, cryptoKey) {
+  //   const jwe = await encrypt(data, cryptoKey);
+  //   return { jwe, data };
+  // }
+
+  // export const axiosProxy = axios.create(Object.assign({}, {
+  //   headers: {'Content-Type': 'application/json;charset=UTF-8'},
+  //   transformRequest: [
+  //     async function (data) {
+  //       const transformedData = await transformData(data, jwk);
+  //       return JSON.stringify(transformedData);
+  //     }
+  //   ]
+  // });
+
+  // function greeter(name) {
+  //   return `Hallo ${name}`;
+  // }
+
+  return {
+    // jwk2cryptoKey,
+    // transformData,
+    // greeter,
+    adder(x, y) { return x + y; },
+  };
 };
+
+// const WebCrypto = require('node-webcrypto-ossl');
+
+// const webcrypto = new WebCrypto();
+// global.crypto = webcrypto;
+
+// import axios from 'axios';
+// const Jose = require('jose-jwe-jws');
+// const { Jose, JoseJWE } = require('jose-jwe-jws');
+// import { Jose, JoseJWE } from 'jose-jwe-jws';
+// import SecretCredentialEncryptor from './secretCredentialEncrypter';
--- src/index.test.js
+++ src/index.test.js
@@ -1,23 +1,62 @@
+// require("babel-polyfill");
 var expect = require('chai').expect;
-var asdJwk = require('../dist/index');
+var td = require('testdouble')
 
-describe('asdJwk', function () {
+const WebCrypto = require('node-webcrypto-ossl');
+const webcrypto = new WebCrypto();
+global.crypto = webcrypto;
 
-  describe('encrypt', function () {
-    it('should be a function', function () {
-      expect(asdJwk.encrypt).to.be.a('function');
-    });
-    it('should return a string', function () {
-      expect(asdJwk.encrypt()).to.be.a('string');
-    });
-  });
+const { Jose, JoseJWE } = require('jose-jwe-jws');
 
-  describe('generateKey', function() {
-    it('should be a function', function () {
-      expect(asdJwk.generateKey).to.be.a('function');
-    });
-    it('should return a string', function () {
-      expect(asdJwk.generateKey()).to.be.a('string');
-    });
-  })
+const myLib = require('../dist/index');
+console.log(myLib);
+const { adder } = myLib({Jose, JoseJWE});
+// const { jwk2cryptoKey, transformData, adder } = myLib({Jose, JoseJWE});
+
+const jwk = {
+  kty: 'RSA',
+  e: 'AQAB',
+  x5t: 'jeylBXdohTJCn6a87uvMHj-p7_k',
+  kid: 'imdznevy-3vujwmkq7vyrgwhubzrm3-cylz36-zy9l4',
+  x5c: [
+    {}
+  ],
+  n: 'yrotRAjpvGfhCnR0Sf0pyU4cxAyDVH8li_9zU1FDlkjNlrGdlHDHn2PwiUz1h2kwi4ZeEoaGn2_qcQjbk7_KpGFyHxE_32Lsknk04YKjqp3BeNqDUDBXUBqZR7qKGQYxeY7UK6z38iRPguLPN8e2u98wJJPkeIf3J4c4748Agj72TWCW2AOdnaFPry_qSERAKTrCTNmj_w5Q-Z6sLfIsuNwFpfnXN7_0qlup9y0ez9EBcqoF-WNxkMrVRMeswwjSLSjjQxoLxVKbJ98yIxrjbS0--XWG6laDf1Wn0LPn4IoJIpfDdnnMbkFsbmpGr_Wa5p7YTOUShgEkmiqeRQ446w'
+}
+const testData = JSON.stringify({
+  bla: 'hi',
+  blub: 5,
 });
+
+describe('adder', function () {
+  it('should calculate the sum of 2 numbers', function () {
+    expect(adder(3,5)).to.equal(8);
+  });
+});
+
+// describe.skip('asb-jwk', function () {
+//   describe('jwk2cryptoKey', function () {
+//     it('should be a function', function () {
+//       expect(jwk2cryptoKey).to.be.a('function');
+//     });
+//     it('should return object', async function () {
+//       const ck = await jwk2cryptoKey(jwk);
+//       expect(ck).to.be.a('object');
+//     });
+//   });
+//   describe('transformData', function () {
+//     it('should be a function', function () {
+//       expect(transformData).to.be.a('function');
+//     });
+//     it('should return object with required properties', async function () {
+//       const ck = await jwk2cryptoKey(jwk);
+//       ck.usages.push('encrypt');
+//       console.log(ck);
+//       const transformedData = await transformData(testData, ck);
+//       console.log(transformedData);
+//       expect(transformedData).to.be.a('object');
+//       expect(transformedData).to.have.property('jwe');
+//       expect(transformedData).to.have.property('data');
+//     })
+//   });
+// });
